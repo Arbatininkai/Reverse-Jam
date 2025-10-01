d38d89914214996d02370a0f0f3ecccd05da80ef
EmilijaVera <egeguzyte@gmail.com>
2025-10-01 16:39:33 +0300

    Create lobby endpointas, LobbyStore ir UserStore laikina, sukurus duomenu bazes bus keiciama

---
+++ API/API/Controllers/LobbyController.cs
@@ -0,0 +1,41 @@
+﻿using API.Models;
+using API.Stores;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LobbyController : ControllerBase
+    {
+        [Authorize] // tik prisijungę per Google gali kurti lobby
+        [HttpPost("create")]
+        public IActionResult CreateLobby([FromBody] Lobby? options)
+        {
+            if (options == null)
+                return BadRequest("Options required");
+
+            // paimam prisijungusio user info iš tokeno
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            var creator = UserStore.Users.FirstOrDefault(u => u.Id.ToString() == userId);
+
+            var newLobby = new Lobby
+            {
+                Id = LobbyStore.Lobbies.Count > 0 ? LobbyStore.Lobbies.Max(l => l.Id) + 1 : 1,
+                Private = options.Private, //is frontendo gaunamos reiksmes
+                AiRate = options.AiRate,
+                HumanRate = options.HumanRate
+            };
+
+            if (creator != null)
+                newLobby.Players.Add(creator);
+
+            LobbyStore.Lobbies.Add(newLobby);
+
+            return Ok(newLobby);
+        }
+    }
+}
---
+++ API/API/Stores/LobbyStore.cs
@@ -0,0 +1,9 @@
+﻿using API.Models;
+
+namespace API.Stores
+{
+    public class LobbyStore
+    {
+        public static List<Lobby> Lobbies { get; set; } = new List<Lobby>(); //laikinas lobiu saugojimas, reiks pakeist duom baze
+    }
+}
