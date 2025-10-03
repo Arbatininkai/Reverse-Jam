c4a2029bf963a31da391e1f412d4ce87524716df
Kęstutis Karneckas <kestutis.karneckas@mif.stud.vu.lt>
2025-10-03 17:31:23 +0300

    Lobby sql

--- API/API/Controllers/LobbyController.cs
+++ API/API/Controllers/LobbyController.cs
@@ -3,6 +3,11 @@ using API.Stores;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+
+using Microsoft.EntityFrameworkCore;
+using API.Data;
+using System.Data.SqlClient;
 
 namespace API.Controllers
 {
@@ -10,32 +15,92 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class LobbyController : ControllerBase
     {
-        [Authorize] // tik prisijungę per Google gali kurti lobby
+        private readonly string _connString;
+
+        public LobbyController(IConfiguration configuration)
+        {
+            //Console.WriteLine(configuration.GetConnectionString("ConnectionString"));
+            _connString = configuration.GetConnectionString("DefaultConnection");
+        }
+        
+        //[Authorize] // tik prisijungę per Google gali kurti lobby
         [HttpPost("create")]
         public IActionResult CreateLobby([FromBody] Lobby? options)
         {
             if (options == null)
                 return BadRequest("Options required");
 
-            // paimam prisijungusio user info iš tokeno
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            var email = User.FindFirstValue(ClaimTypes.Email);
-            var creator = UserStore.Users.FirstOrDefault(u => u.Id.ToString() == userId);
+            if (options.Token == "")
+            {
+                return BadRequest("Token required");
+            }
+            
+            if (string.IsNullOrEmpty(_connString))
+            {
+                return BadRequest("Connection string is missing!");
+            }
+
+            //Paimam tokena
+            var token = options.Token;
+            
+            //Sql connectionas
+            using var conn = new SqlConnection(_connString);
+            conn.Open();
+            
+            //Patikrinam ar tokenas valid
+            string tokenQuery = "SELECT 1 FROM users WHERE gid = @token";
+            using var tokenCmd = new SqlCommand(tokenQuery, conn);
+            tokenCmd.Parameters.AddWithValue("@token", token);
+
+            if (tokenCmd.ExecuteScalar() == null)
+            {
+                return BadRequest("User authentication failed");
+            }
+            
+            Random rnd = new Random();
+            int lobCode = 0;
+            
+            
 
             var newLobby = new Lobby
             {
                 Id = LobbyStore.Lobbies.Count > 0 ? LobbyStore.Lobbies.Max(l => l.Id) + 1 : 1,
                 Private = options.Private, //is frontendo gaunamos reiksmes
                 AiRate = options.AiRate,
-                HumanRate = options.HumanRate
+                HumanRate = options.HumanRate,
+                LobbyCode = lobCode
             };
+            
+            newLobby.Players.Add(token);
 
-            if (creator != null)
-                newLobby.Players.Add(creator);
+            while (true)
+            {
+                lobCode = rnd.Next(1000, 9999);
+                string lobcQuery = "SELECT 1 FROM lobby WHERE lobbyCode = @lobCode";
+                using var lobCmd = new SqlCommand(lobcQuery, conn);
+                lobCmd.Parameters.AddWithValue("@lobCode", lobCode);
 
-            LobbyStore.Lobbies.Add(newLobby);
+                if (lobCmd.ExecuteScalar() == null)
+                {
+                    break;
+                }
+            }
 
-            return Ok(newLobby);
+            //Issaugom lobby sql
+            string createQuery = "INSERT INTO lobby (lobbyCode, isPrivate, isAiRate, isHumanRate, creator, players) VALUES(@lobbyCode, @isPrivate, @isAiRate, @isHumanRate, @creator, @players)";
+            using var createCmd = new SqlCommand(createQuery, conn);
+            createCmd.Parameters.AddWithValue("@lobbyCode", lobCode);
+            createCmd.Parameters.AddWithValue("@isPrivate", newLobby.Private);
+            createCmd.Parameters.AddWithValue("@isAiRate", newLobby.AiRate);
+            createCmd.Parameters.AddWithValue("@isHumanRate", newLobby.HumanRate);
+            createCmd.Parameters.AddWithValue("@creator", token);
+            createCmd.Parameters.AddWithValue("@players", token);
+
+
+            createCmd.ExecuteScalar();
+            //LobbyStore.Lobbies.Add(newLobby);
+
+            return Ok(lobCode);
         }
     }
 }
--- API/API/Models/Lobby.cs
+++ API/API/Models/Lobby.cs
@@ -3,13 +3,15 @@
     public class Lobby
     {
         public int Id { get; set; }
-        public string LobbyCode { get; set; } = Guid.NewGuid().ToString("N")[..6].ToUpper(); // random 6 simbolių kodas
+        public int LobbyCode { get; set; } // random 4 simbolių kodas
         
         //defaultines reiksmes, is frontendo bus galima gaut kitokias reiksmes
         public bool Private { get; set; } = false;
         public bool AiRate { get; set; } = true;
         public bool HumanRate { get; set; } = true;
+        
+        public string Token { get; set;}
 
-        public List<User> Players { get; set; } = new List<User>();
+        public List<String> Players { get; set; } = new List<String>();
     }
 }
