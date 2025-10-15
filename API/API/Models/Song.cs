c8c37718c785ba328cd3b43b537d42f5182d966d
EmilijaVera <egeguzyte@gmail.com>
2025-10-15 13:10:43 +0300

    feat: add random-song endpoint (GET api/songs/getsong)

---
+++ API/API/Models/Song.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Models
+{
+    public class Song
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;    // Azure blob URL
+        public string? Artist { get; set; } 
+    }
+}
---
+++ API/API/Stores/SongStore.cs
@@ -0,0 +1,14 @@
+﻿using API.Models;
+using System.Collections.Generic;
+
+namespace API.Stores
+{
+    public static class SongStore
+    {
+        public static List<Song> Songs { get; set; } = new List<Song>
+        {
+            new Song { Name = "Crazy", Url = "https://songstorage25.blob.core.windows.net/songs/Gnarls Barkley - Crazy.wav", Artist = "Gnarls Barkley" },
+            new Song { Name = "Billie Jean", Url = "https://songstorage25.blob.core.windows.net/songs/Michael Jackson - Billie Jean (Official Video).wav", Artist = "Michael Jackson" }
+        };
+    }
+}
