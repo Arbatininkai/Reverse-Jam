1ba8ff8ce2df5c2b356b1201819b12e83182294d
EmilijaVera <egeguzyte@gmail.com>
2025-10-21 22:40:59 +0300

    Implementation of .NET interface IEquatable to compare songs by URL

--- API/API/Models/Song.cs
+++ API/API/Models/Song.cs
@@ -1,10 +1,16 @@
 ﻿namespace API.Models
 {
-    public struct Song //struct usage second task
+    public struct Song : IEquatable<Song> //struct usage second task, implement IEquatable for tenth task
     {
         public string Name { get; set; }
         public string Url { get; set; } // Azure blob URL
         public string? Artist { get; set; }
         public string? CoverUrl { get; set; }
+        public bool Equals(Song other) //IEquatable usage to compare songs by URL
+        {
+            return string.Equals(Url, other.Url, StringComparison.OrdinalIgnoreCase);
+        }
+        public override int GetHashCode()
+            => Url?.GetHashCode() ?? 0;
     }
 }
